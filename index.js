6b3da33157d948f9a0d66490b82d2c7366a27f70
Nathan Bakirci <nathan@virtru.com>
2018-03-20 21:30:44 -0400

    Printing premiums

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
-const bittrex = require('node-bittrex-api');
+/* eslint-disable no-console */
+
+const Bittrex = require('node-bittrex-api');
+const Binance = require('node-binance-api');
+const Promise = require('bluebird');
+const keys = require('./keys.json');
+
+const bittrex = Promise.promisifyAll(Bittrex);
+const binance = Promise.promisifyAll(Binance);
+
+binance.options({
+  APIKEY: keys.binance.key,
+  APISECRET: keys.binance.secret,
+  useServerTime: true, // If you get timestamp errors, synchronize to server time at startup
+  test: true, // If you want to use sandbox mode where orders are simulated
+});
 
 bittrex.options({
-  'apikey' : API_KEY,
-  'apisecret' : API_SECRET,
+  apikey: keys.bittrex.key,
+  apisecret: keys.bittrex.secret,
+  inverse_callback_arguments: true,
 });
 
-bittrex.getmarketsummaries( function( data, err ) {
-  if (err) {
-    return console.error(err);
-  }
-  for( var i in data.result ) {
-    bittrex.getticker( { market : data.result[i].MarketName }, function( ticker ) {
-      console.log( ticker );
-    });
-  }
-});
+function getPrices() {
+  return Promise.all([
+    bittrex.gettickerAsync({ market: 'USDT-BTC' }),
+    binance.bookTickersAsync('BTCUSDT'),
+  ]).then(res => ({
+    bittrex: {
+      bid: res[0].result.Bid,
+      ask: res[0].result.Ask,
+    },
+    binance: {
+      bid: res[1].bidPrice,
+      ask: res[1].askPrice,
+    },
+  })).catch((err) => {
+    console.log(err);
+  });
+}
+
+// Check prices
+setInterval(() => {
+  const datetime = new Date().toISOString();
+  getPrices().then((prices) => {
+    const premiums = {
+      bittrex: (((prices.bittrex.bid / prices.binance.ask) - 1) * 100).toFixed(2),
+      binance: (((prices.binance.bid / prices.bittrex.ask) - 1) * 100).toFixed(2),
+    };
+    console.log(`[${datetime}] Bittrex Premium: ${premiums.bittrex}%, Binance Premium: ${premiums.binance}%`);
+  });
+}, 5000);
